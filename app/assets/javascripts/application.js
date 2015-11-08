bd74bd6f05f3458debef58d5707ed3b8c2bacee5
Philippe Aka <philippeaka@outlook.com>
2015-11-08 16:24:58 -0500

    setup css/sass environment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
+//= require cocoon
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
